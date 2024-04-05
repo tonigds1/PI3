a4152118890d338d02d8efbd096c3fa71d7bb878
Antônio Guedes <146470610+tonigds1@users.noreply.github.com>
2024-04-05 19:44:22 -0300

    Add files via upload

---
+++ PI3/Modulo.java
@@ -0,0 +1,96 @@
+import java.util.ArrayList;
+
+public class Modulo {
+    private int idModulo;
+    private String titulo;
+    private String aula;
+    private String atividadeModulo;
+    private String conteudo;
+    private Curso curso;
+    private ArrayList<Aula> aulas;
+    private Teste teste;
+
+    public Modulo(int idModulo, String titulo, String aula, String atividadeModulo, String conteudo, Curso curso, ArrayList<Aula> aulas, Teste teste){
+        this.idModulo = idModulo;
+        this.titulo = titulo;
+        this.aula = aula;
+        this.atividadeModulo = atividadeModulo;
+        this.conteudo = conteudo;
+        this.curso = curso;
+        this.aulas = aulas;
+        this.teste = teste;
+    }
+
+    public Modulo(){}
+
+    public int getIdModulo() {
+        return idModulo;
+    }
+    public void setIdModulo(int idModulo) {
+        this.idModulo = idModulo;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAula() {
+        return aula;
+    }
+    public void setAula(String aula) {
+        this.aula = aula;
+    }
+
+    public String getAtividadeModulo() {
+        return atividadeModulo;
+    }
+    public void setAtividadeModulo(String atividadeModulo) {
+        this.atividadeModulo = atividadeModulo;
+    }
+
+    public String getConteudo() {
+        return conteudo;
+    }
+    public void setConteudo(String conteudo) {
+        this.conteudo = conteudo;
+    }
+
+    public Curso getCurso() {
+        return curso;
+    }
+    public void setCurso(Curso curso) {
+        this.curso = curso;
+    }
+
+    public ArrayList<Aula> getAulas() {
+        return aulas;
+    }
+    public void setAulas(ArrayList<Aula> aulas) {
+        this.aulas = aulas;
+    }
+
+    public Teste getTeste() {
+        return teste;
+    }
+    public void setTeste(Teste teste) {
+        this.teste = teste;
+    }
+
+    public void vizualizarConteudo(){
+
+    }
+
+    public boolean marcarConclusaoModulo(){
+        
+        return marcarConclusaoModulo();
+    }
+
+    public double avaliarModulo(){
+        
+        return avaliarModulo();
+    }
+    
+}
---
+++ PI3/Professor.java
@@ -0,0 +1,64 @@
+import java.util.ArrayList;
+
+public class Professor {
+    private int idProfessor;
+    private String nome;
+    private String sobrenome;
+    private String professor;
+    private String areaDeConhecimento;
+    private ArrayList<Aula> aulas;
+
+    private Professor(int idProfessor, String nome, String sobrenome, String professor, String areaDeConhecimento, ArrayList<Aula> aulas){
+        this.idProfessor = idProfessor;
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+        this.professor = professor;
+        this.areaDeConhecimento = areaDeConhecimento;
+        this.aulas = aulas;
+    }
+
+    private Professor(){}
+
+    public int getIdProfessor() {
+        return idProfessor;
+    }
+    public void setIdProfessor(int idProfessor) {
+        this.idProfessor = idProfessor;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public String getProfessor() {
+        return professor;
+    }
+    public void setProfessor(String professor) {
+        this.professor = professor;
+    }
+
+    public String getAreaDeConhecimento() {
+        return areaDeConhecimento;
+    }
+    public void setAreaDeConhecimento(String areaDeConhecimento) {
+        this.areaDeConhecimento = areaDeConhecimento;
+    }
+    
+    public ArrayList<Aula> getAulas() {
+        return aulas;
+    }
+    public void setAulas(ArrayList<Aula> aulas) {
+        this.aulas = aulas;
+    }
+   
+}
